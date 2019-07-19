cccf5af13f288a256ea76cca856e2d3c64e41c58
alexisjcarr <acarr59@gmail.com>
2019-07-19 11:10:08 -0500

    ➕ Adds GET /api/actions
     #1

--- routers/actionsRouter.js
+++ routers/actionsRouter.js
@@ -2,4 +2,13 @@ const router = require("express").Router();
 
 const db = require("../data/models/actionsModel");
 
+router.get("/", async (req, res) => {
+  try {
+    const actions = await db.find();
+    res.status(200).json(actions);
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
+});
+
 module.exports = router;
